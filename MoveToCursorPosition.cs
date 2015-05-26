95496be0f8154c3891418c38634802424d4a376c
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-05-26 17:00:42 +0200

    Add licence info

--- MoveToCursorPosition.cs
+++ MoveToCursorPosition.cs
@@ -1,4 +1,7 @@
-﻿#define DEBUG_LOGGER
+﻿// Copyright (c) 2015 Bartlomiej Wolk (bartlomiejwolk@gmail.com)
+//  
+// This file is part of the MoveToCursorPosition extension for Unity.
+// Licensed under the MIT license. See LICENSE file in the project root folder.
 
 using FileLogger;
 using UnityEngine;
--- Utilities.cs
+++ Utilities.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿// Copyright (c) 2015 Bartlomiej Wolk (bartlomiejwolk@gmail.com)
+//  
+// This file is part of the MoveToCursorPosition extension for Unity.
+// Licensed under the MIT license. See LICENSE file in the project root folder.
+
+using System;
 using System.Diagnostics;
 using System.Reflection;
 #if UNITY_EDITOR
